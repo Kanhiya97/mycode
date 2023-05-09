633858ccaf45c55721de3d6d30fe8c9e48f47479
kanhiya97 <kskanhiyasingh@gmail.com>
2023-05-08 17:04:25 -0700

    this is my core java code

---
+++ fsjd-jan-batch-main (1)/fsjd-jan-batch-main/src/controlStatements/conditional/ifElseDemos/ConditionalStatements1.java
@@ -0,0 +1,20 @@
+package controlStatements.conditional.ifElseDemos;
+
+public class ConditionalStatements1 {
+    public static void main(String[] args) {
+        if(true){
+            System.out.println("execute this");
+            System.out.println("execute this");
+            System.out.println("execute this");
+            System.out.println("execute this");
+            System.out.println("execute this");
+        }
+
+        if(false){
+            System.out.println("Skip it");
+            System.out.println("Skip it");
+            System.out.println("Skip it");
+        }
+
+    }
+}
---
+++ fsjd-jan-batch-main (1)/fsjd-jan-batch-main/src/controlStatements/looping/forDemos/Demo7.java
@@ -0,0 +1,9 @@
+package controlStatements.looping.forDemos;
+
+public class Demo7 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i=i+2) {
+            System.out.print(i+" ");
+        }
+    }
+}
